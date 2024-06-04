4aa70399e957600d0ed294d81c769e4f7c4a4622
James Pitt <39842510+James-Pitt-dev@users.noreply.github.com>
2024-06-03 20:09:59 -0700

    integrated Williams changes to latest version

--- routes/exercises.js
+++ routes/exercises.js
@@ -3,37 +3,15 @@ const router = express.Router({mergeParams: true});
 const catchAsync = require('../utils/catchAsync');
 const ExpressError = require('../utils/ExpressError');
 const { storeReturnTo } = require('../middleware');
+const Exercise = require('../models/exercise');
 
 
 // routes for exercises
 
-const workoutAPI = async function(filter){
-// /exercises/exercise/{id}
-    const url = `https://exercisedb.p.rapidapi.com/exercises${filter}`;
-    const options = {
-        method: 'GET',
-        headers: {
-            'X-RapidAPI-Key': 'a34bd91f76mshd066cdaa2775237p1f279djsn703a2c619030',
-            'X-RapidAPI-Host': 'exercisedb.p.rapidapi.com'
-        }
-    };
-    
-    try {
-        const response = await fetch(url, options);
-        const result = await response.json();
-        console.log(result);
-        return result;
-    } catch (error) {
-        console.error(error);
-    }
- }
-
 // GET route to show all exercises from the database
 router.get('/', catchAsync(async (req, res) => {
-    // const exercises = await Exercise.find({});
-    // res.render('exercises/index', { exercises });
-const workouts = await workoutAPI('?limit=1000');
-res.render('exercises/index', {workouts});
+    const workouts = await Exercise.find({});
+    res.render('exercises/index', {workouts});
 }));
 
 router.get('/:id', catchAsync(async (req, res) => {
--- routes/workouts.js
+++ routes/workouts.js
@@ -57,3 +57,38 @@ router.get('/index', catchAsync(async (req, res) => {
 }));
 
 module.exports = router;
+
+// const fetch = require('node-fetch');
+// const exerciseCatalog = require('../seeds/exerciseCatalogData')
+
+// router.get('/viewAllExercise', async function (req, res) {
+//   const json_file = exerciseCatalog.exerciseCatalogList
+//   if (json_file.length == 0) {
+//     const url = 'https://exercisedb.p.rapidapi.com/exercises?limit=20&offset=0';
+//     const bodyParturl = 'https://exercisedb.p.rapidapi.com/exercises/bodyPartList';
+//     const options = {
+//       method: 'GET',
+//       headers: {
+//         'x-rapidapi-key': '955929d1b2msh162f5c1d94afdf3p19c01fjsndb67889a007f',
+//         'x-rapidapi-host': 'exercisedb.p.rapidapi.com'
+//       }
+//     };
+
+//     try {
+//       const response = await fetch(url, options);
+//       const result = await response.json();
+//       console.log(result);
+//       const exerciseList = result
+//       res.render('../views/workouts/index', { exerciseList });
+
+//     } catch (error) {
+//       console.error(error);
+//     }
+//   } else {
+//     console.log("DATA ALREADY EXISTED")
+//     console.log(json_file)
+//     const exerciseList = json_file
+//     res.render('../views/workouts/index', { exerciseList });
+
+//   }
+// });
