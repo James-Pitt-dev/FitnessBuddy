559d35b715bcc725edee3218f66dd94c626c9149
James Pitt <39842510+James-Pitt-dev@users.noreply.github.com>
2024-06-27 14:45:01 -0700

    Updated the /chat view message boxes

--- routes/aiTrainer.js
+++ routes/aiTrainer.js
@@ -5,19 +5,17 @@ const WorkoutExercise = require("../models/workoutExercise");
 const Exercise = require("../models/exercise");
 const catchAsync = require("../utils/catchAsync");
 const { isLoggedIn } = require("../middleware");
-const sendPrompt = require('../openAIApi');
+const sendPrompt = require('../public/javascripts/openAIApi');
 const ChatHistory = require('../models/chatHistory');
 
 // GET Chat Interface
 router.get('/chat', isLoggedIn, catchAsync(async (req, res) => {
-    // TODO: fetch past convos to pass to view; put isLoggedIn middleware; 
-  
         const id = req.user._id
         const pastMessages = await ChatHistory.find({author: id}).populate('author').exec();
         res.render('aiTrainer/chat', {pastMessages});
 }));
 
-router.post('/chat', isLoggedIn, async (req, res) => {
+router.post('/chat', isLoggedIn, catchAsync(async (req, res) => {
     const {userChat} = req.body.AI;
     const data = await sendPrompt(userChat);
     
@@ -28,8 +26,7 @@ router.post('/chat', isLoggedIn, async (req, res) => {
     });
 
     await chatHistory.save();
-    console.log(chatHistory);
     res.json(chatHistory);
-});
+}));
 
 module.exports = router;
